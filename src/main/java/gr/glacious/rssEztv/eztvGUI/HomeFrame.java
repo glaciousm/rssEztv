f0d9a244bdd95d1df590ab58e4abd5049e2f0855
Glacious <mmamouzellos@gmail.com>
2017-06-25 22:30:45 +0300

    Added functionality: Hide to tray

--- src/main/java/gr/glacious/rssEztv/eztvGUI/HomeFrame.java
+++ src/main/java/gr/glacious/rssEztv/eztvGUI/HomeFrame.java
@@ -1,8 +1,17 @@
 package gr.glacious.rssEztv.eztvGUI;
 
+import java.awt.AWTException;
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -17,6 +26,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
+import javax.swing.UIManager;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -25,8 +35,10 @@ import gr.glacious.rssEztv.model.FeedMessage;
 import gr.glacious.rssEztv.util.OpenPage;
 import gr.glacious.rssEztv.util.TxtEditor;
 
-public class HomeFrame {
+public class HomeFrame extends HideToSystemTray {
 
+	private static final long serialVersionUID = 5078700827741826934L;
+	
 	File file = new File("C:\\Users\\Glacious\\Documents\\favorites.txt");
 	File dFile = new File("C:\\Users\\Glacious\\Documents\\downloaded.txt");
 
@@ -34,6 +46,8 @@ public class HomeFrame {
 	String selected;
 	FeedMessage series;
 	TxtEditor txtEditor;
+	TrayIcon trayIcon;
+    SystemTray tray;
 
 	public JFrame frame;
 
@@ -71,13 +85,14 @@ public class HomeFrame {
 		JList<String> rssJList = new JList<>(listModel);
 		JScrollPane scrollPane = new JScrollPane();
 		JButton btnDownload = new JButton("Download");
+		btnDownload.setEnabled(false);
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setEnabled(false);
 
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
-		
 
 		rssList = rssExecute.getRSS();
 		txtEditor = new TxtEditor();
@@ -96,21 +111,20 @@ public class HomeFrame {
 		}
 
 		// create the list
-		
+
 		rssJList.addListSelectionListener(new ListSelectionListener() {
-			
+
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
 				btnDownload.setEnabled(true);
-				
+				btnDelete.setEnabled(true);
+
 			}
 		});
 		scrollPane.setBounds(10, 10, 414, 190);
 		scrollPane.setViewportView(rssJList);
 		frame.getContentPane().add(scrollPane);
 
-		
-		btnDownload.setEnabled(false);
 		btnDownload.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				for (FeedMessage feedMessage : rssList) {
@@ -143,7 +157,7 @@ public class HomeFrame {
 							}
 
 						}
-						
+
 					}
 
 				}
@@ -152,6 +166,40 @@ public class HomeFrame {
 
 		btnDownload.setBounds(10, 211, 89, 23);
 		frame.getContentPane().add(btnDownload);
+
+		btnDelete.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String selected = rssJList.getSelectedValue();
+				txtEditor = new TxtEditor();
+
+				try {
+					txtEditor.writeToFile(selected, dFile);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+				listModel.clear();
+				txtEditor = new TxtEditor();
+				for (FeedMessage rss : rssList) {
+					for (int i = 0; i < txtEditor.readFromFile(file).size(); i++) {
+						if (rss.getTitle().startsWith(txtEditor.readFromFile(file).get(i))) {
+							if (!txtEditor.readFromFile(dFile).contains(rss.getTitle())) {
+								listModel.addElement(rss.getTitle());
+
+							}
+
+						}
+					}
+
+				}
+
+			}
+
+		});
+		btnDelete.setBounds(109, 211, 89, 23);
+		frame.getContentPane().add(btnDelete);
 
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
@@ -185,20 +233,20 @@ public class HomeFrame {
 			}
 		});
 		mnFavorites.add(mntmFavoriteList);
-		
+
 		JMenuItem mntmHistoryList = new JMenuItem("History List");
-		
+
 		mntmHistoryList.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				HistoryList historyList = new HistoryList(menuBar);
 				frame.setVisible(false);
 				historyList.frame.setVisible(true);
-				
+
 			}
 		});
-		
+
 		mnFavorites.add(mntmHistoryList);
 
 		JMenu mnHelp = new JMenu("Help");
@@ -206,6 +254,80 @@ public class HomeFrame {
 
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mnHelp.add(mntmAbout);
+		
+		//Hide to tray
+		
+		System.out.println("creating instance");
+        try{
+            System.out.println("setting look and feel");
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e){
+            System.out.println("Unable to set LookAndFeel");
+        }
+        if(SystemTray.isSupported()){
+            System.out.println("system tray supported");
+            tray=SystemTray.getSystemTray();
+
+            Image image=Toolkit.getDefaultToolkit().getImage("C:\\Users\\Glacious\\Pictures\\favicon.png");
+            ActionListener exitListener=new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Exiting....");
+                    System.exit(0);
+                }
+            };
+            PopupMenu popup=new PopupMenu();
+            MenuItem defaultItem=new MenuItem("Exit");
+            defaultItem.addActionListener(exitListener);
+            popup.add(defaultItem);
+            defaultItem=new MenuItem("Open");
+            defaultItem.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                	frame.setVisible(true);
+                	frame.setExtendedState(JFrame.NORMAL);
+                }
+            });
+            popup.add(defaultItem);
+            trayIcon=new TrayIcon(image, "SystemTray Demo", popup);
+            trayIcon.setImageAutoSize(true);
+        }else{
+            System.out.println("system tray not supported");
+        }
+        frame.addWindowStateListener(new WindowStateListener() {
+            public void windowStateChanged(WindowEvent e) {
+                if(e.getNewState()==ICONIFIED){
+                    try {
+                        tray.add(trayIcon);
+                        frame.setVisible(false);
+                        System.out.println("added to SystemTray");
+                    } catch (AWTException ex) {
+                        System.out.println("unable to add to tray");
+                    }
+                }
+        if(e.getNewState()==7){
+                    try{
+            tray.add(trayIcon);
+            frame.setVisible(false);
+            System.out.println("added to SystemTray");
+            }catch(AWTException ex){
+            System.out.println("unable to add to system tray");
+        }
+            }
+        if(e.getNewState()==MAXIMIZED_BOTH){
+                    tray.remove(trayIcon);
+                    frame.setVisible(true);
+                    System.out.println("Tray icon removed");
+                }
+                if(e.getNewState()==NORMAL){
+                    tray.remove(trayIcon);
+                    frame.setVisible(true);
+                    System.out.println("Tray icon removed");
+                }
+            }
+        });
+        setIconImage(Toolkit.getDefaultToolkit().getImage("Duke256.png"));
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 }
